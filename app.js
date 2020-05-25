05613e21cdf0afdc5305b7eb59f1344e2ca5ac7a
udaya2308 <udaya14apr2000@gmail.com>
2020-05-25 11:53:08 +0530

    get gedcode for given location

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ request({ url: url, json: true }, (error, response) => {
     const feel_like = response.body.current.feelslike
     console.log('the temperature is %s .It feels like %s', current_degree, feel_like)
 })
+
+const geoCode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidWRheWEwNCIsImEiOiJja2FtM2EzdHMwMnhsMnNwZnd5eGxncjl3In0.uFvUDHi0YJJ2xns-12kHiQ&limit=1'
+request({ url: geoCode_url, json: true }, (error, response) => {
+    const lat = response.body.features[0].center[1]
+    const long = response.body.features[0].center[0]
+    console.log('Latitude: %s Longitude: %s', lat, long)
+})

a7683d722811380eb74396b6ede140af42afbf33
udaya2308 <udaya14apr2000@gmail.com>
2020-05-25 14:52:38 +0530

    callback chaining

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
 
-geocode.geocode('Los angels', (error, data) => {
-    console.log('Error', error)
-    console.log(data)
-})
+const location = process.argv[2]
+
+if (location) {
+    geocode.geocode(location, (error, data) => {
+        if (error)
+            return console.log(error)
+
+        forecast(data.Latitude, data.Longitude, (error, forecastdata) => {
+            if (error)
+                return console.log(error)
+            console.log(data.location)
+            console.log(forecastdata)
+        })
+    })
+} else {
+    console.log('Please provide location')
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=dac82d5b47b0a993572c994661db2c66&query=' + latitude + ',' + longitude + '&units=s'
+    const url = 'http://api.weatherstack.com/current?access_key=dac82d5b47b0a993572c994661db2c66&query=' + latitude + ',' + longitude + '&units=m'
     request({ url: url, json: true }, (error, response) => {
         if (error) {
             callback("Unable to connect to weather service!", undefined)
@@ -10,10 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             const current_degree = response.body.current.temperature
             const feel_like = response.body.current.feelslike
-            const data = {
-                temperature: current_degree,
-                feelslike: feel_like
-            }
+            const data = response.body.current.weather_descriptions[0] + '\nThe temperature is ' + current_degree + '. It feels like ' + feel_like
             callback(undefined, data)
         }
     })

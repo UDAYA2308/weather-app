3ffda52d6e316d3bd1fc4258548cdf02dd184c72
udaya2308 <udaya14apr2000@gmail.com>
2020-05-25 12:07:58 +0530

    error handling

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const request = require('postman-request')
 const url = 'http://api.weatherstack.com/current?access_key=dac82d5b47b0a993572c994661db2c66&query=37.8267,-122.4233&units=s'
 request({ url: url, json: true }, (error, response) => {
-    const current_degree = response.body.current.temperature
-    const feel_like = response.body.current.feelslike
-    console.log('the temperature is %s .It feels like %s', current_degree, feel_like)
+    if (error) {
+        console.log("Unable to connect to weather service!")
+    } else if (response.body.error) {
+        console.log("Unable to find location")
+    } else {
+        const current_degree = response.body.current.temperature
+        const feel_like = response.body.current.feelslike
+        console.log('The temperature is %s .It feels like %s', current_degree, feel_like)
+    }
 })
 
 const geoCode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidWRheWEwNCIsImEiOiJja2FtM2EzdHMwMnhsMnNwZnd5eGxncjl3In0.uFvUDHi0YJJ2xns-12kHiQ&limit=1'
 request({ url: geoCode_url, json: true }, (error, response) => {
-    const lat = response.body.features[0].center[1]
-    const long = response.body.features[0].center[0]
-    console.log('Latitude: %s Longitude: %s', lat, long)
+    if (error) {
+        console.log("Unable to connect to mapbox service!")
+    } else if (response.body.features.length == 0) {
+        console.log("Unable to find location")
+    } else {
+        const lat = response.body.features[0].center[1]
+        const long = response.body.features[0].center[0]
+        console.log('Latitude: %s Longitude: %s', lat, long)
+
+    }
 })
